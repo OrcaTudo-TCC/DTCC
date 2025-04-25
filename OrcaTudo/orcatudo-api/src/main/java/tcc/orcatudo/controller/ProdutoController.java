631d834f79abd8f41fc0a7ed56d95141300b5f87
Murilo M <murilomiransa5@gmail.com>
2025-04-25 00:08:42 -0300

    add endpoints de subcategoriafinal e produto

--- OrcaTudo/orcatudo-api/src/main/java/tcc/orcatudo/controller/ProdutoController.java
+++ OrcaTudo/orcatudo-api/src/main/java/tcc/orcatudo/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import tcc.orcatudo.dtos.PutProdutoDTO;
 import tcc.orcatudo.dtos.SaveProdutoDTO;
 import tcc.orcatudo.entitites.Produto;
@@ -25,8 +31,13 @@ public class ProdutoController {
     @Autowired
     ProdutoService produtoService; 
 
+    @Operation(summary = "Retorna o Produto por Nome ou Id", description = "<h3>Retorna o produto correspondente ao Nome ou ao Id, caso nenhum dos dois seja passado retorna uma lista com todos os produtos<br>Exemplo de requisição: \"\\produtos\\4\"</h3>")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Retornou o(s) produto(s) com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Id ou Nome não possui correspondência", content = @Content())
+    })
     @GetMapping()
-    public List<Produto> getProduto(@RequestParam(required = false) Integer id , @RequestParam(required =  false) String nome ){
+    public List<Produto> getProduto(@Parameter(required = false, description = "Id do produto")@RequestParam(required = false) Integer id ,@Parameter(required = true, description = "Nome do produto") @RequestParam(required =  false) String nome ){
         if (id != null) {
             return List.of(produtoService.getProdutoById(id));
         }else if(nome != null){
@@ -35,24 +46,51 @@ public class ProdutoController {
         return produtoService.getAllProduto();
     }
 
+    @Operation(summary = "Retorna lista de produtos correspondete a subcategoria final", description = "<h3>Retorna uma lista com todos os produtos da subcategoria passada pelo caminho da requisição<br>Exemplo:\"\\produtos\\subcategoriafinal\\chaves_de_fenda\"</h3>")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Retornou a lista de produtos com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Nome da Subcategoria não possui correspondência, não encontrado", content = @Content())
+    })
     @GetMapping("/subcategoriafinal/{nome}")
-    public List<Produto> getProdutoBySubcategoriaFinal(@PathVariable String nome){
+    public List<Produto> getProdutoBySubcategoriaFinal(@Parameter(required = true, description = "Nome da Subcategoria final")@PathVariable String nome){
         return produtoService.getProdutoBySubcategoriaFinal(nome);
-
     }
 
+    @Operation(summary = "Retorna lista de produtos correspondente a um fornecedor", description = "<h3>Retorna uma lista com todos os produtos de um forneceodr passado pelo caminho da requisição<br>Exemplo:\"produtos\\fornecedor\\casa_dos_pregos\"</h3>")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Retornou a lista de produtos com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Nome da Subcategoria não possui correspondência, não encontrado", content = @Content())
+    })
     @GetMapping("/fornecedor/{nome}")
-    public List<Produto> getProdutoByFornecedor(@PathVariable String nome){
+    public List<Produto> getProdutoByFornecedor(@Parameter(required = true,description = "Nome do Fornecedor")@PathVariable String nome){
         return produtoService.getProdutoByFornecedor(nome);
     }
 
+    @Operation(summary = "Atualiza os atributos de um produto", description = "<h3>Atualiza um produto conforme o valro dos atributos passado via corpo da requisição.</h3>")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Atualizou o produto ocm sucesso"),
+        @ApiResponse(responseCode = "404", description = "Id do produto não possui correspondência", content = @Content()),
+        @ApiResponse(responseCode = "400", description = "Bad Resquest: Há algo errado na requisição", content = @Content())
+    })
     @PutMapping()
     public ResponseEntity<Produto> putProduto(@RequestBody PutProdutoDTO produto){
         return ResponseEntity.ok(produtoService.putProduto(produto));
     }
 
+    @Operation(summary = "Cria um Produto", description = "<h3>Cria um Produto conforme o valor dos atributos no corpo da requisição</h3>")
     @PostMapping()
     public ResponseEntity<Produto> saveProduto(@RequestBody SaveProdutoDTO produto){
         return ResponseEntity.ok(produtoService.saveProduto(produto));
     }
+
+    @Operation(summary = "Deleta um produto pelo ID", description = "Deleta o produto correspondente ao Id passado pelo caminho da requisição")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Retornou a lista de produtos com sucesso", content = @Content()),
+        @ApiResponse(responseCode = "404", description = "Nome da Subcategoria não possui correspondência, não encontrado", content = @Content())
+    })
+    @DeleteMapping("/{id}")
+    public void deleteProduto(@Parameter(required = true, description = "Id do produto")@PathVariable int id){
+        produtoService.deleteProdutoById(id);
+    }
+
 }
--- OrcaTudo/orcatudo-api/src/main/java/tcc/orcatudo/controller/SubcategoriaFinalController.java
+++ OrcaTudo/orcatudo-api/src/main/java/tcc/orcatudo/controller/SubcategoriaFinalController.java
@@ -9,17 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import tcc.orcatudo.dtos.SubcategoriaDTO;
 import tcc.orcatudo.dtos.SubcategoriaFinalDTO;
+import tcc.orcatudo.entitites.Subcategoria;
 import tcc.orcatudo.entitites.SubcategoriaFinal;
 import tcc.orcatudo.services.SubcategoriaFinalService;
 
@@ -40,22 +43,73 @@ public class SubcategoriaFinalController {
     public List<SubcategoriaFinal> getSubcategoriaFinalBySubcategoria(@Parameter(required = true, description = "Nome da Subcategoria")@PathVariable String subcategoria){
         return subFinalService.getSubcategoriaFinalBySubcategoriaNome(subcategoria);
     }
-
-    
-    @PutMapping("/{idSubcategoriaFinal}/nome")
-    public ResponseEntity<SubcategoriaFinal> putSubcategoriaFinal(@PathVariable int idSubcategoriaFinal, @RequestBody String novoNome){
-        return ResponseEntity.ok(subFinalService.updateNomeSubcategoriaFinal(idSubcategoriaFinal , novoNome));
+    //-------------------------------------------------------------------------------------------------
+        @Operation(
+        summary = "Edita o nome da subcategoria final",
+        description = "<h3>Edita a subcategoria final correspondente ao Id passado pelo caminho da requisição<br>o novo nome deve ser passado pelo corpo da requição sem aspas</h3>",
+        requestBody = @RequestBody(
+            required = true,
+            description = "Novo nome sem aspas",
+            content = @Content(schema = @Schema(example = "novo nome"))
+        )
+    )
+    @ApiResponses({
+        @ApiResponse( responseCode = "200", description = "Editou o nome da subcategoria final com sucesso", content = @Content(schema = @Schema(implementation = Subcategoria.class))),
+        @ApiResponse(
+            responseCode = "404",
+            description = "Não encontrou nenhuma subcategoria final com o id passado",
+            content = @Content()
+        )
+    })
+    @PutMapping("/{id}/nome")
+    public ResponseEntity<SubcategoriaFinal> putSubcategoriaFinal(@Parameter(required = true, description = "id da subcategoria final")@PathVariable int id, @RequestBody String novoNome){
+        return ResponseEntity.ok(subFinalService.updateNomeSubcategoriaFinal(id , novoNome));
     }
+    //-----------------------------------------------------------------------------------------------
+    @Operation(
+        summary = "Edita a Subcategoria a qual a Subcategoria final pertence",
+        description = "<h3>Edita a Subcategoria a qual a Subcategoria final pertence, Por meio do caminho da requisição deverá ser passado o id da Subcategoria final<br> e por do corpo da requisição o id da Subcategoria</h3>",
+        requestBody = @RequestBody(
+            required = true,
+            description = "<h3>Id da Subcategoria</h3>"
+        ))
+    @ApiResponses({
+        @ApiResponse( responseCode = "200", description = "Editou a Subcategoria da Subcategoria final com sucesso", content = @Content(schema = @Schema(implementation = Subcategoria.class))),
+        @ApiResponse(
+            responseCode = "404",
+            description = "Não encontrou nenhuma Subcategoria final ou Subcategoria com o id passado",
+            content = @Content()
+        )
+    })
     @PutMapping("/{idSubcategoriaFinal}/update_subcategoria")
-    public ResponseEntity<SubcategoriaFinal> putSubcategoriaFinal(@PathVariable int idSubcategoriaFinal, @RequestBody int idSubcategoria){
+    public ResponseEntity<SubcategoriaFinal> putSubcategoriaFinal(@Parameter(required = true, description = "Id da Subcategoria final") @PathVariable int idSubcategoriaFinal, @RequestBody int idSubcategoria){
         return ResponseEntity.ok(subFinalService.updateSubcategoria(idSubcategoriaFinal , idSubcategoria));
     }
+    //-----------------------------------------------------------------------
+    @Operation(
+        summary = "Cria uma Subcategoria final",
+        description = "<h3>Cria uma Subcategoria final com os atributos passado via corpo da requisição</h3>",
+        requestBody = @RequestBody(
+            required = true,
+            description = "<h3>Atributos da subcategoria final</h3>",
+        content = @Content(schema = @Schema(implementation = SubcategoriaFinalDTO.class))
+    ))
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Subcategoria final criada com sucesso", content = @Content()),
+        @ApiResponse(responseCode = "400", description = "Bad request, tem alguma coisa errada na requisição", content = @Content())
+    })
     @PostMapping()
     public ResponseEntity<SubcategoriaFinal> postSubcategoriaFinal(@RequestBody SubcategoriaFinalDTO dto){
         return ResponseEntity.ok(subFinalService.postSubcategoriaFinal(dto));
     }
+
+    @Operation(summary = "Deleta uma Subcategoria final pelo id", description = "<h3>Deleta a subcategoria final correspondente ao id passado via caminho da requisição</h3>")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Subcategoria final deletada com sucesso", content = @Content()),
+        @ApiResponse(responseCode = "404", description = "Nenhuma Subcategoria final corresponde ao id passado", content = @Content())
+    })
     @DeleteMapping("/{id}")
-    public void deleteSubcategoriaFinal(@PathVariable int id){
+    public void deleteSubcategoriaFinal(@Parameter(required = true, description = "Id da Subcategoria final") @PathVariable int id){
         subFinalService.deleteByid(id);
     }
 }
