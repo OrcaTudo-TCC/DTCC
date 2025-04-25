1237aee47566ed7cdd4fbbb4eb527bf5a5218a9b
Murilo M <murilomiransa5@gmail.com>
2025-04-25 17:49:39 -0300

    add documentação para endpoint de carrinho e item carrinho

--- OrcaTudo/orcatudo-api/src/main/java/tcc/orcatudo/controller/CarrinhoController.java
+++ OrcaTudo/orcatudo-api/src/main/java/tcc/orcatudo/controller/CarrinhoController.java
@@ -8,10 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import tcc.orcatudo.dtos.PostCarrinhoDTO;
 import tcc.orcatudo.entitites.Carrinho;
@@ -19,21 +27,40 @@ import tcc.orcatudo.services.CarrinhoService;
 
 @RestController
 @RequestMapping("/carrinho")
+@Tag(name = "Carrinho")
 public class CarrinhoController {
 
     @Autowired
     CarrinhoService carrinhoService;
 
+    @Operation(summary = "Retorna um carrinho", description = "<h3>Retorna o carrinho correspondente ao id do usuário passado pelo caminho da requisição<br>Exemplo: \"/carrinho/7 \"</h3>")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Retornou com sucesso o carrinho", content = @Content(schema = @Schema(implementation = Carrinho.class))),
+        @ApiResponse(responseCode = "404", description = "Nenhuma correspondência ao id do Usuário passado", content = @Content())
+    })
     @GetMapping("/{idUsuario}")
-    public Carrinho getCarrinhoByUsuarioId(@PathVariable int idUsuario){
+    public Carrinho getCarrinhoByUsuarioId(@Parameter(required = true, description = "Id do Usuário")@PathVariable int idUsuario){
         return carrinhoService.getCarrinhoByUsuarioId(idUsuario);
     }
 
+    @Operation(summary = "Atualiza o Status do carrinho", description = "Atualiza o status do carrinho correspondente ao usuário")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Atualizou com sucesso o carrinho", content = @Content(schema = @Schema(implementation = Carrinho.class))),
+        @ApiResponse(responseCode = "404", description = "Nenhuma correspondência ao id do Usuário passado", content = @Content())
+    })
     @PutMapping("/{idUsuario}")
-    public ResponseEntity<Carrinho> putCarrinhoById(@PathVariable int idUsuario, @RequestParam boolean status){
+    public ResponseEntity<Carrinho> putCarrinhoById(@Parameter(required = true, description = "Id do Usuário")@PathVariable int idUsuario, @RequestParam boolean status){
         return ResponseEntity.ok(carrinhoService.changeCarrinhoStatus(idUsuario ,status));
     }
 
+    @Operation(
+        summary = "Cria um carrinho",
+        description = "Cria um carrinho com os atributos passados via corpo da requisição",
+        requestBody = @RequestBody(
+            required = true,
+            description = "Atributos necessários para criar carrinho",
+            content = @Content(schema = @Schema(implementation = PostCarrinhoDTO.class))
+        ))
     @PostMapping()
     public ResponseEntity<Carrinho> postCarrinho(@RequestBody PostCarrinhoDTO carrinhoDTO){
         return ResponseEntity
--- OrcaTudo/orcatudo-api/src/main/java/tcc/orcatudo/controller/ItemCarrinhoController.java
+++ OrcaTudo/orcatudo-api/src/main/java/tcc/orcatudo/controller/ItemCarrinhoController.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import tcc.orcatudo.dtos.PostItemCarrinhoDTO;
 import tcc.orcatudo.dtos.PutItemCarrinhoDTO;
@@ -19,21 +27,69 @@ import tcc.orcatudo.services.ItemCarrinhoService;
 
 @RestController
 @RequestMapping("/item-carrinho")
+@Tag( name = "Carrinho", description = "Operações do carrinho e relacionados")
 public class ItemCarrinhoController {
 
     @Autowired
     ItemCarrinhoService itemCarrinhoService;
 
+    @Operation(summary = "Retorna os Itens de um carrinho", description = "<h3>Retorna Uma lista de Itens correspondente ao id do carrinho passado via caminho da requisição.<br>Exemplo: \" /item-carrinho/7 \"</h3>")
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            description = "Retornou os itens com sucesso",
+            content = @Content(schema = @Schema(implementation = ItemCarrinho.class))
+        ),
+        @ApiResponse(
+            responseCode = "404",
+            description = "O id do carrinho não possui correspondência",
+            content = @Content()
+        )
+    })
     @GetMapping("/{id}")
-    public List<ItemCarrinho> getItemByCarrinhoId(@PathVariable int id){
+    public List<ItemCarrinho> getItemByCarrinhoId(@Parameter(required = true, description = "Id do Carrinho")@PathVariable int id){
         return itemCarrinhoService.getItemByCarrinhoId(id);
     }
 
+    @Operation(
+        summary = "Atualiza um Item-Carrinho",
+        description = "<h3>Atualiza um Item-Carrinho com os atributos passados pelo corpo da requisição</h3>",
+        requestBody = @RequestBody(
+            required = true,
+            description = "Atributos necessários para utualizar item",
+            content = @Content(schema = @Schema(implementation = PutItemCarrinhoDTO.class))
+        ))
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            description = "Atualizou o Item com sucesso",
+            content = @Content(schema = @Schema(implementation = ItemCarrinho.class))
+        ),
+        @ApiResponse(
+            responseCode = "404",
+            description = "O id do Item ou do Carrinho não possui correspondência",
+            content = @Content()
+        )
+    })
     @PutMapping()
     public ResponseEntity<ItemCarrinho> putItemCarrinho(@RequestBody PutItemCarrinhoDTO itemToUpdate){
         return ResponseEntity.ok(itemCarrinhoService.putItemCarrinho(itemToUpdate));
     }
     
+
+    @Operation(summary = "Cria um Item-carrinho",description = "<h3>Cria um Item-carrinho com os atributos passados via corpo da requisição</h3>")
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            description = "Criou um Item-Carrinho com sucesso",
+            content = @Content()
+        ),
+        @ApiResponse(
+            responseCode = "404",
+            description = "O id do Carrinho não possui correspondência",
+            content = @Content()
+        )
+    })
     @PostMapping()
     public void postItemCarrinho(@RequestBody PostItemCarrinhoDTO postItemCarrinhoDTO){
         itemCarrinhoService.postItemCarrinho(postItemCarrinhoDTO);
