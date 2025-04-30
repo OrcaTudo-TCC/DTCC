ffc967390bd59ae3780590185402890f05d64e58
Murilo M <murilomiransa5@gmail.com>
2025-04-30 17:15:28 -0300

    fix get Fornecedor , add post fornecedor

--- OrcaTudo/js/Fornecedor.js
+++ OrcaTudo/js/Fornecedor.js
@@ -1,12 +1,12 @@
 
 const endpoint = "http://localhost:8080/fornecedor"
 
- async function getFornecedor(id){
+async function getFornecedor(id){
 
      if(typeof id === 'number' && Number.isInteger(id)){
         // se a requisição conter parametro numérico
         try{
-            const response = await fetch(endpoint, { method: "POST" });
+            const response = await fetch(endpoint, { method: "GET" });
             const data = await response.json();
 
             if(!response.ok){
@@ -20,7 +20,7 @@ const endpoint = "http://localhost:8080/fornecedor"
     }else if(typeof id === 'string'){
         // se a requisição conter uma String
         try{
-            const response = await fetch(endpoint,{ method: "POST"});
+            const response = await fetch(endpoint,{ method: "GET"});
             const data = await response.json();
 
             if(!response.ok){
@@ -49,5 +49,37 @@ const endpoint = "http://localhost:8080/fornecedor"
     }
 
     
+}
+
+// deve se enviar um objeto fornecedor com todos os atributos e valores
+async function postFornecedor(fornecedor) {
+    const camposObrigatorios = [
+        'nome',
+        'password',
+        'razao_social',
+        'email',
+        'documento',
+        'telefone',
+        'endereco',
+        'areaDeAtuacao',
+        'descricao',
+        'avaliacao'
+      ];
+    // Verifica se é um objeto
+    if (typeof dados !== 'object' || dados === null) {
+        console.log('Parâmetro inválido: não é um objeto.');
+        return;
+      }
+    
+    // Verifica se todos os campos obrigatórios estão presentes
+    const todosPresentes = camposObrigatorios.every(campo => campo in dados);
+    if (!todosPresentes) {
+        console.log('Objeto incompleto. Faltando algum campo.');
+        return;
+    }
+    const response = await fetch(endpoint,{ method: "POST" });
+    const data = await response.json()
+    return data;
+
 }
 getFornecedor()
