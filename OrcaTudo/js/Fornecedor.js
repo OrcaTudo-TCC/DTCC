b2a6f947298edf8dacbd904422037406ce7677ae
Murilo M <murilomiransa5@gmail.com>
2025-04-30 16:27:52 -0300

    add criando get na classe utilitária Fornecedor

--- OrcaTudo/js/Fornecedor.js
+++ OrcaTudo/js/Fornecedor.js
@@ -1,12 +1,53 @@
 
 const endpoint = "http://localhost:8080/fornecedor"
 
-function getFornecedor(){
-    fetch(endpoint)
-    .then(res=>res.json())
-    .then(res=>{
-        console.log(res)
-    }).catch(err=>console.log("Erro na requisição: "+ err))
-}
+ async function getFornecedor(id){
 
+     if(typeof id === 'number' && Number.isInteger(id)){
+        // se a requisição conter parametro numérico
+        try{
+            const response = await fetch(endpoint, { method: "POST" });
+            const data = await response.json();
+
+            if(!response.ok){
+                throw new Error("Erro na requisição: "+ response.status);
+            }
+            return data;
+        }catch(err){
+            console.log(err);
+            return data;
+        }
+    }else if(typeof id === 'string'){
+        // se a requisição conter uma String
+        try{
+            const response = await fetch(endpoint,{ method: "POST"});
+            const data = await response.json();
+
+            if(!response.ok){
+                throw new Error("Erro na requisição: "+ response.status);
+            }
+            return data;
+        }catch(err){
+            console.log(err);
+            return null;
+        }
+    // se a requisição não conter nennhum parametro
+    }else if(typeof id === undefined){
+        try{
+            const response = await fetch(endpoint);
+            const data = await response.json();
+    
+            console.log(data);
+            return data
+        }catch(err){
+            console.log(err)
+            return null
+        }
+    }else{
+        console.log("Requisição com parâmetro inválido");
+        return null;
+    }
+
+    
+}
 getFornecedor()
