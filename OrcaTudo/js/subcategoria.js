6d3d6a9808a7d2651fabbf7dd519f2c716288ded
Joao Pereira Menezes <joao.pereira.menezes@g.globo>
2025-05-09 12:05:15 -0300

    criação classes 09-05

--- OrcaTudo/js/subcategoria.js
+++ OrcaTudo/js/subcategoria.js
@@ -106,6 +106,92 @@ async function putSubcategorianome(id, nome) {
       }
 
     }
+
+    async function getSubcategoriafinal (subcategoria) {
+      try{
+        const response = await fetch(endpoint + "/" + subcategoria, {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+          
+        });
+
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        const data = await response.json();
+        return data;
+
+      } catch (error) {
+        console.log ("Erro na Requisição getSubcategoriafinal: " + error);
+      }
+    }
+
+    async function postSubcategoriafinal () {
+      try{
+        const response = await fetch(endpoint,{
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify ({
+
+            "nome": nome,
+            "nomeDasubcategoriafinal": subcategoriafinal
+
+          })});
+
+          if (!response.ok) {
+            throw new Error(response.status);
+          }
+          const data = await response.json();
+          return data;
+
+      } catch (error) {
+        console.log ("Erro na Requisição postSubcategoriafinal: " + error);
+      }
+    }
+
+    async function putSubcategoriafinal (id, nome) {
+      try {
+        const response = await fetch(endpoint + "/" + id + "/nome",{
+          method:"PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify ({nome:nome})
+        });
+
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        const data = await response.json();
+        return data;
+
+      } catch (error) {
+        console.log ("Erro na Requisição putSubcategoriafinal: " + error);
+      }
+
+    }
+
+    async function deleteSubcategoriafinal (id) {
+      try{
+        const response = await fetch(endpoint + "/" + id, {
+          method:"DELETE",
+          headers: { "Content-Type": "application/json" },         
+        });
+
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+      
+        return true;
+
+      }  catch (error) {
+        console.log ("Erro na Requisição deleteSubcategoria: " + error);
+
+      }
+    }
+
+    
+    
+
+    
   
   
     
