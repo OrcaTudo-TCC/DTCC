34ebbdbf43983ae3e38f85bf4e98d1158e5ad830
MURILO MIRANDA VERCOSA <MURILO_4478@uirapuru.br>
2025-05-26 20:29:25 -0300

    adicionando classe de decotificação

--- OrcaTudo/js/authUsuario.js
+++ OrcaTudo/js/authUsuario.js
@@ -34,8 +34,19 @@ async function login(email, password) {
   }
 }
 
+//Método usado para decotificar o token e tranformá-lo em JSON
 
+function decodificarJWT(token) {
+  const base64Url = token.split('.')[1];
+  if (!base64Url) throw new Error('JWT inválido');
 
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(atob(base64).split('').map(c =>
+    '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+  ).join(''));
+
+  return JSON.parse(jsonPayload);
+}
 
 
 
