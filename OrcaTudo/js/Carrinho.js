b91036e696c83a9e3079279fcd38490f51bfbbb6
Murilo M <murilomiransa5@gmail.com>
2025-06-08 22:15:25 -0300

    finalizei a integração da tela de cotação

--- OrcaTudo/js/Carrinho.js
+++ OrcaTudo/js/Carrinho.js
@@ -2,35 +2,39 @@
 const endpointCarrinho = "http://localhost:8080/carrinho";
 
 async function getCarrinho(idUsuario) {
-    try{
-        if(typeof idUsuario !== 'number' || !Number.isInteger(idUsuario)){
+    try {
+        if (typeof idUsuario !== 'number' || !Number.isInteger(idUsuario)) {
             throw new Error("O id do usuário precisa ser um inteiro");
         }
 
-        const response = await fetch(endpointCarrinho + "/" + idUsuario,{
+        const response = await fetch(endpointCarrinho + "/" + idUsuario, {
             method: "GET",
             headers: { "Content-Type": "application/json" }
         })
-        if(!response.ok){
-            throw new Error("Erro na requisição"+response.status)
+        if (!response.ok) {
+            if (response.status === 404) {
+                console.warn("Carrinho não encontrado para o usuário com ID " + idUsuario);
+                return null;
+            }
+            throw new Error("Erro na requisição" + response.status)
         }
         const data = await response.json();
 
         return data;
 
-    }catch(err){
-        console.log("Erro no método get carrinho: "+ err)
-        return;
+    } catch (err) {
+        console.log("Erro no método get carrinho: " + err)
+        return [];
     }
 }
 
 async function postCarrinho(emailUsuario) {
     try {
-        if(typeof emailUsuario !== 'string'){
+        if (typeof emailUsuario !== 'string') {
             throw new Error("O email do usuário precisa ser uma string");
         }
 
-        const response = await fetch(endpointCarrinho+ "/"+ emailUsuario,{
+        const response = await fetch(endpointCarrinho + "/" + emailUsuario, {
             method: "POST",
             headers: { "Content-Type": "application/json" }
         })
@@ -41,7 +45,7 @@ async function postCarrinho(emailUsuario) {
 
         return data;
     } catch (err) {
-        console.log("Erro na requisição post carrinho: "+ err)
+        console.log("Erro na requisição post carrinho: " + err)
         return;
     }
 }
